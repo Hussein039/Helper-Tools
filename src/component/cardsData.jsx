03e994ab90785fcfc002d4f535b645c4d5d3020e
Hussein039 <84193192+Hussein039@users.noreply.github.com>
2023-11-20 22:16:38 -0500

    adding an image

--- src/component/cardsData.jsx
+++ src/component/cardsData.jsx
@@ -3,14 +3,14 @@ const cardsData = [
       id: 'card1',
       name: 'Text Extractor',
       description: 'This tool extracts text data from images.',
-      image: '/src/assets/images/services/img-to-text.png',
+      image: './src/assets/images/services/img-to-text.png',
       link: './products/ImageUpload.jsx'
     },
     {
       id: 'card2',
       name: 'Another Card',
       description: 'Description for another card.',
-      image: '/src/assets/images/services/background-remover.png',
+      image: './src/assets/images/services/background-remover.png',
       link: '/'
     },
     {
